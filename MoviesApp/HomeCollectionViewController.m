60a7a4f3c90b92a7c60e347c6f54e3c06cf0716e
Kerolloz <kerolloz@yahoo.com>
2018-09-05 16:02:25 +0200

    add HTTP request permission

--- MoviesApp/HomeCollectionViewController.m
+++ MoviesApp/HomeCollectionViewController.m
@@ -8,8 +8,13 @@
 
 #import "HomeCollectionViewController.h"
 #import "MovieDetailsViewController.h"
+#import <SDWebImage/UIImageView+WebCache.h>
 
 @interface HomeCollectionViewController ()
+
+
+@property CGFloat width;
+@property CGFloat height;
 
 @end
 
@@ -21,27 +26,15 @@ static NSString * const reuseIdentifier = @"Cell";
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations
-    // self.clearsSelectionOnViewWillAppear = NO;
     
-    
-    // Do any additional setup after loading the view.
+    self.width = [UIScreen mainScreen].bounds.size.width/2;
+    self.height = [UIScreen mainScreen].bounds.size.height/3;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 #pragma mark <UICollectionViewDataSource>
 
@@ -58,11 +51,11 @@ static NSString * const reuseIdentifier = @"Cell";
     UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
     
     UIImageView *image = [cell viewWithTag:1];
-
-    CGFloat width = [UIScreen mainScreen].bounds.size.width/2;
-    CGFloat height = [UIScreen mainScreen].bounds.size.height/3;
-    image.frame = CGRectMake(0, 0, width, height);
-    [image setImage:[UIImage imageNamed:@"movie.png"]];
+    image.frame = CGRectMake(0, 0, self.width, self.height);
+    NSString *moviePosterUrl = @"http://image.tmdb.org/t/p/w185/nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg";
+    [image sd_setImageWithURL:[NSURL URLWithString:moviePosterUrl]
+                 placeholderImage:[UIImage imageNamed:@"movie.png"]];
+  
     // Configure the cell
     
     return cell;
@@ -70,9 +63,7 @@ static NSString * const reuseIdentifier = @"Cell";
 
 -(CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath{
    
-    CGFloat width = [UIScreen mainScreen].bounds.size.width/2;
-    CGFloat height = [UIScreen mainScreen].bounds.size.height/3;
-    return CGSizeMake(width, height);
+    return CGSizeMake(self.width, self.height);
 }
 
 -(void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
